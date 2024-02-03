b6d0419981e01f160364857b49d785e57065d2b6
bumbuk477 <154671639+bumbuk477@users.noreply.github.com>
2024-02-03 13:30:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 app = QApplication([])
 from main_window import *
 from random import choice,shuffle
-
+from time import sleep
 class Question:
     def __init__(self,question,answer,wrong_answer1,wrong_answer2,wrong_answer3):
         self.question = question
@@ -13,26 +14,36 @@ class Question:
         self.attemps = 0
         self.correct = 0
     def got_right(self):
+        global correct,attempts
         self.attemps += 1
         self.correct += 1
-        print("Це правильна відповідь!")
+        #print("Це правильна відповідь!")
     
 
     def got_wrong(self):
+        global attempts
         self.attemps += 1
-        print("Відповідь невірна")
+        #print("Відповідь невірна")
 
-def new_question(question,radio_list):
+def new_question(question):
     random_question = choice(question)
     text_qwestion.setText(random_question.question)
     right_answer.setText(random_question.answer)
-    answer = radio_list[0]
-    wrong_answer1,wrong_answer2,wrong_answer3 = radio_list[1],radio_list[2],radio_list[3]
+    
     answer.setText(random_question.answer)
     wrong_answer1.setText(random_question.wrong_answer1)
     wrong_answer2.setText(random_question.wrong_answer2)
     wrong_answer3.setText(random_question.wrong_answer3)
     return random_question
+def check_result():
+    global answer, wrong_answer1,wrong_answer2,wrong_answer3
+    print(answer.isChecked())
+    if answer.isChecked():
+        text_result.setText("Відповідь вірна!")
+        random_question.got_right()
+    else:
+        text_result.setText("Відповідь не вірна!")
+        random_question.got_wrong()
 
 
 def swich_screen():
@@ -40,12 +51,22 @@ def swich_screen():
     if btn_answer.text()== "Відповісти":
         qwestion_group.hide()
         answer_group.show()
+        check_result()
         btn_answer.setText("Наступне питання")
     else:
+        random_question = new_question(question)
         qwestion_group.show()
         answer_group.hide()
         btn_answer.setText("Відповісти")
-        random_question = new_question(question,radio_list)
+def rest():
+    main_win.hide()
+    t=timer.value()*1000*60
+    T.setInterval(t)
+    T.start()
+
+def wake_up():
+    T.stop()
+    main_win.show()
 
 
 
@@ -57,6 +78,10 @@ question = [q1,q2,q3]
 
 radio_list = [rbtn1,rbtn2,rbtn3,rbtn4]
 shuffle(radio_list)
+answer = radio_list[0]
+wrong_answer1,wrong_answer2,wrong_answer3 = radio_list[1],radio_list[2],radio_list[3]
+
+
 
 main_win = QWidget()
 main_win.resize(600,500)
@@ -65,10 +90,15 @@ main_win.move(300,300)
 
 
 main_win.setLayout(line)
+T = QTimer()
 
-
-random_question = new_question(question,radio_list)
+random_question = new_question(question)
 btn_answer.clicked.connect(swich_screen)
+btn_sleep.clicked.connect(rest)
+T.timeout.connect(wake_up)
+
+
+
 main_win.show()
 
 
